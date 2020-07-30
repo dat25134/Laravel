664d32452f9ac97df6d14b66a1a799e7f3cd83c0
dat25134 <tlvdat.bkdn@gmail.com>
2020-07-30 08:51:14 +0700

    update bài tập

--- Giohang/resources/views/products/index.blade.php
+++ Giohang/resources/views/products/index.blade.php
@@ -3,9 +3,12 @@
 @section('content')
 <div class="container-fluid pt-5">
     <div class="d-flex bd-highlight mb-3">
-        <div class="mr-auto p-2 bd-highlight"><h2 class="text-center">DANH SÁCH SẢN PHẨM</h2></div>
-        <div class="p-2 bd-highlight"><a href="{{route('showcart')}}"> Xem chi tiết giỏ hàng <i class="fa fa-shopping-cart btn btn-primary rounded-circle" aria-hidden="true"></i></a></div>
-      </div>
+        <div class="mr-auto p-2 bd-highlight">
+            <h2 class="text-center">DANH SÁCH SẢN PHẨM</h2>
+        </div>
+        <div class="p-2 bd-highlight"><a href="{{route('showcart')}}"> Xem chi tiết giỏ hàng <i
+                    class="fa fa-shopping-cart btn btn-primary rounded-circle" aria-hidden="true"></i></a></div>
+    </div>
 
     <div class="container mt-5">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5">
@@ -13,12 +16,13 @@
             <div class="card text-center mt-3 border ml-5 rounded pt-2" style="width: 18rem;">
                 <img src="{{$item->image}}" class="card-img-top" alt="{{$item->name}}">
                 <div class="card-body">
-                  <h5 class="card-title">{{ $item->name}}</h5>
-                  <p class="card-text">{{ number_format($item->price)." VNĐ"}}</p>
-                  <a href="{{route('show',$item->id)}}" class="btn btn-primary rounded-pill">Details</a>
-                  <button onclick="addcart({{$item->id}})" class="btn btn-danger rounded-pill" style="width:100px"><i class="fa fa-shopping-cart" aria-hidden="true"></i></button>
+                    <h5 class="card-title">{{ $item->name}}</h5>
+                    <p class="card-text">{{ number_format($item->price)." VNĐ"}}</p>
+                    <a href="{{route('show',$item->id)}}" class="btn btn-primary rounded-pill">Details</a>
+                    <button onclick="addcart({{$item->id}})" class="btn btn-danger rounded-pill" style="width:100px"><i
+                            class="fa fa-shopping-cart" aria-hidden="true"></i></button>
                 </div>
-              </div>
+            </div>
             @endforeach
         </div>
     </div>
@@ -34,7 +38,11 @@
         type: "POST",
         url: 'http://localhost:8000/'+id+'/addcart',
         }).done(function(message) {
+            toastr.options = {
+                "positionClass": "toast-bottom-right",
+            };
             toastr["success"](message, "Success");
+
         });
     }
 </script>
